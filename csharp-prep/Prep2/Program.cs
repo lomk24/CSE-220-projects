42db0a2bc28b9b0950cce6ef7738ef03c1fd0228
lomk24 <kro22001@byui.edu>
2024-01-13 15:11:05 -0700

    Finished prep 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What was your grade? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80 )
+        {
+            letter = "B";
+        }
+        else if (percent >= 70 )
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations, you've passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry, you'll get it next time.");
+        }
+
+    }   
+
 }
